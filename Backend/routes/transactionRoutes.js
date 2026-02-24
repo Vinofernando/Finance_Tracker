59184240018144ffa953618f521f602a28bd1c23
Vino <vinofernando84@gmail.com>
2026-02-24 23:12:55 +0700

    add summary routes

--- Backend/routes/transactionRoutes.js
+++ Backend/routes/transactionRoutes.js
@@ -1,6 +1,8 @@
 import {
   getUserTransaction,
   newTransaction,
+  deleteTransaction,
+  sumTransactions,
 } from "../controllers/transactionController.js";
 import { authenticatedToken } from "../middleware/authenticatedToken.js";
 import express from "express";
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.get("/", authenticatedToken, getUserTransaction);
 router.post("/add-transaction", authenticatedToken, newTransaction);
+router.delete("/delete-transaction/:id", authenticatedToken, deleteTransaction);
+router.get("/summary", authenticatedToken, sumTransactions);
 
 export default router;
