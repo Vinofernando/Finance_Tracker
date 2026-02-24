3440cb63a5a6d148e73cfa9d3628f22cc104db64
Vino <vinofernando84@gmail.com>
2026-02-24 23:13:44 +0700

    create api to transaction route

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,12 @@
 import authRoutes from "./routes/authRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/transaction", transactionRoutes);
 
 app.use(errorHandler);
 app.listen(5000, () => console.log("Server running on 5000"));
