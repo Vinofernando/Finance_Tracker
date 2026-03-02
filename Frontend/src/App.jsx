b9a211967da534139a13a1bb61f25be6a20e8bbe
Vino <vinofernando84@gmail.com>
2026-03-02 23:03:15 +0700

    git add route to add-transaction

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import AddPages from "./pages/AddTransactions";
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import AuthManager from "./auth/AuthManager";
 
 function App() {
   return (
     <BrowserRouter>
+      <AuthManager />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/add-transaction" element={<AddPages />} />
       </Routes>
     </BrowserRouter>
   );
