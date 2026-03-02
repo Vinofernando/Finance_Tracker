e0986cb6eaaf8838f3e176e785a507cede8697ca
Vino <vinofernando84@gmail.com>
2026-03-02 23:14:57 +0700

    add cors

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,15 @@ import authRoutes from "./routes/authRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 import express from "express";
 import errorHandler from "./middleware/errorHandler.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/transaction", transactionRoutes);
 
