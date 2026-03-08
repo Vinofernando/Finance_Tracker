1f3c9d4c7ae470af2a77d641e5398b2034faf05a
Vino <vinofernando84@gmail.com>
2026-03-08 23:08:33 +0700

    add authorizeRole

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,11 +1,19 @@
-import { login, register } from "../controllers/authControllers.js";
+import { login, register, deleteUser } from "../controllers/authControllers.js";
 import { verifyEmail } from "../controllers/verifyController.js";
+import authorizeRole from "../utils/authorizeRole.js";
+import { authenticatedToken } from "../middleware/authenticatedToken.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/register", register);
-router.get("/verify/:token", verifyEmail);
+router.get("/verify", verifyEmail);
 router.post("/login", login);
+router.delete(
+  "/delete/:userId",
+  authenticatedToken,
+  authorizeRole("admin"),
+  deleteUser,
+);
 
 export default router;
