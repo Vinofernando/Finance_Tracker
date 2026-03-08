1f8fc70c7df1cdd9120afa1b77e12d562f5c36de
Vino <vinofernando84@gmail.com>
2026-03-08 23:09:05 +0700

    add get user route  for admin role

--- Backend/routes/transactionRoutes.js
+++ Backend/routes/transactionRoutes.js
@@ -3,8 +3,10 @@ import {
   newTransaction,
   deleteTransaction,
   sumTransactions,
+  getUser,
 } from "../controllers/transactionController.js";
 import { authenticatedToken } from "../middleware/authenticatedToken.js";
+import authorizeRole from "../utils/authorizeRole.js";
 import express from "express";
 
 const router = express.Router();
@@ -13,5 +15,6 @@ router.get(`/`, authenticatedToken, getUserTransaction);
 router.post("/add-transaction", authenticatedToken, newTransaction);
 router.delete("/delete-transaction/:id", authenticatedToken, deleteTransaction);
 router.get("/summary", authenticatedToken, sumTransactions);
+router.get("/users", authenticatedToken, authorizeRole("admin"), getUser);
 
 export default router;
