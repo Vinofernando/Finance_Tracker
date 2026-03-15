6a38446b7e633620876360278e15cc3aec8be848
Vino <vinofernando84@gmail.com>
2026-03-15 15:21:26 +0700

    refactor pool

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 import pkg from "pg";
 
+import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
