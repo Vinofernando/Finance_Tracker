f974b8c997b670ccc54a53f07297ca14a53cf7d9
Vino <vinofernando84@gmail.com>
2026-03-15 12:53:15 +0700

    add error message

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,10 +6,15 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true,
-    sslmode: "verify-full",
-  },
+  ssl: process.env.DATABASE_URL.includes("localhost")
+    ? false // Matikan SSL jika konek ke DB lokal
+    : {
+        rejectUnauthorized: false, // Seringkali dibutuhkan untuk cloud DB jika tidak punya file CA
+      },
 });
 
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 export default pool;
