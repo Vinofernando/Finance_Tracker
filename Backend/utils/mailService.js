d2530ea34e3ecd68ea7e48a979ccdb659cab7552
Vino <vinofernando84@gmail.com>
2026-03-15 15:00:36 +0700

    remove trycatch block

--- Backend/utils/mailService.js
+++ Backend/utils/mailService.js
@@ -20,13 +20,12 @@ export const sendVerification = async (toEmail, token) => {
     process.env.FRONTEND_URL || "https://FinanceTrackerV.netlify.app";
   const link = `${baseUrl}/success-verification?token=${token}`;
 
-  try {
-    const info = await resend.emails.send({
-      from: `Finance tracker`, // Lebih terlihat profesional
-      to: toEmail,
-      subject: "Verifikasi Email Anda",
-      // Gunakan tanda kutip pada href untuk mencegah error HTML
-      html: `
+  await resend.emails.send({
+    from: `Finance tracker`, // Lebih terlihat profesional
+    to: toEmail,
+    subject: "Verifikasi Email Anda",
+    // Gunakan tanda kutip pada href untuk mencegah error HTML
+    html: `
       <div style="font-family: sans-serif; line-height: 1.5;">
         <p>Halo,</p>
         <p>Silakan klik tombol di bawah ini untuk memverifikasi email Anda. Link ini akan kedaluwarsa dalam 15 menit:</p>
@@ -37,9 +36,5 @@ export const sendVerification = async (toEmail, token) => {
         <p><small>${link}</small></p>
       </div>
     `,
-    });
-    console.log("Email sent:", info.response);
-  } catch (error) {
-    console.error("Email error:", error);
-  }
+  });
 };
