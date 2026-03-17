a710d08f4dbec48f4320d18c58302b96e79c2861
Vino <vinofernando84@gmail.com>
2026-03-17 23:10:26 +0700

    add domain cors

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,20 @@ import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://finance-tracker.store",
-  }),
-);
+
+const cors = require("cors");
+
+const corsOptions = {
+  origin: [
+    "https://finance-trackerv.netlify.app",
+    "https://finance-tracker.store",
+    "http://localhost:5173", // tetap izinkan local untuk testing
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 app.use(morgan("combined"));
