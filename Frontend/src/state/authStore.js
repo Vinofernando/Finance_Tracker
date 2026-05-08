6b7ff25491c91048e8ed9a33489305ca5eecd711
Vino <vinofernando84@gmail.com>
2026-05-08 21:04:57 +0700

    add global state using zustand

---
+++ Frontend/src/state/authStore.js
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  username: "",
+  email: "",
+  password: "",
+  error: null,
+  isAddModalOpen: false,
+
+  // Action untuk update state
+  setUsername: (name) => set({ username: name }),
+  setEmail: (mail) => set({ email: mail }),
+  setPassword: (pass) => set({ password: pass }),
+  setError: (err) => set({ error: err }),
+  toggleAddModal: () =>
+    set((state) => ({
+      isAddModalOpen: !state.isAddModalOpen,
+    })),
+
+  // Action untuk reset data (misal setelah submit berhasil)
+  clearAuthData: () =>
+    set({
+      username: "",
+      email: "",
+      password: "",
+      error: "",
+      isAddModalOpen: false,
+    }),
+}));
+
+export default useAuthStore;
