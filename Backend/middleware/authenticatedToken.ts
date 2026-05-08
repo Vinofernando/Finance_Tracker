299e8d014f0a5eef2984c911faa79b24c7349b90
Vino <vinofernando84@gmail.com>
2026-05-08 21:14:10 +0700

    refactor to ts

---
+++ Backend/middleware/authenticatedToken.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
+import type { User } from "../interfaces/interfaces.js";
+const JWT_SECRET = process.env.SECRET_KEY;
+
+interface AuthRequest extends Request {
+  user?: User | string | jwt.JwtPayload;
+}
+export const authenticatedToken = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const header = req.headers["authorization"];
+  const token = header && header.split(" ")[1];
+  if (!token)
+    throw { status: 401, message: "You must login to access this page" };
+
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded as User;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: "Token invalid or expired" });
+  }
+};
+
+export default authenticatedToken;
--- Backend/middleware/errorHandler.ts
+++ Backend/middleware/errorHandler.ts
@@ -1,4 +1,6 @@
-export default function errorHandler(err, req, res, next) {
+import type { ErrorRequestHandler } from "express";
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Internal server error";
 
@@ -6,4 +8,6 @@ export default function errorHandler(err, req, res, next) {
     error: true,
     message,
   });
-}
+
+  next();
+};
