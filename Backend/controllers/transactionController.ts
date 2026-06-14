88e2bf9112086044c3be3f06be90998b1b4ce41e
Vino <vinofernando84@gmail.com>
2026-06-14 16:52:16 +0700

    add planned transaction controller

--- Backend/controllers/transactionController.ts
+++ Backend/controllers/transactionController.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import * as transactionService from "../services/transactionsService.js";
-import type { Request } from "express";
+import type { NextFunction, Request } from "express";
 import type {
   GetUserControl,
   GetUserTransactionControl,
   NewTransactionControl,
   DeleteTransactionControl,
 } from "../interfaces/interfaces.js";
+import predictCatTyp from "../utils/predictCatTyp.js";
+
 export const getUserTransaction = async (
   req: Request,
   res: express.Response,
@@ -108,5 +110,145 @@ export const getUser = async (
     res.json(user);
   } catch (err) {
     return next(err);
+  }
+};
+
+export const predict = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { description } = req.body;
+
+    const resultPred = await predictCatTyp(description);
+    res.json(resultPred);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const planningTransaction = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const resultPlannedTransaction =
+      await transactionService.plannedTransaction({
+        userId: Number(req.user.userId),
+        categoryId: Number(req.body.categoryId),
+        amount: Number(req.body.amount),
+        description: req.body.description,
+        type: req.body.type,
+        startDate: req.body.startDate,
+        frequency: req.body.frequency,
+      });
+
+    res.status(201).json(resultPlannedTransaction);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const userPlanedtransaction = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const { plannedId } = req.query as { plannedId: string | "" };
+    const result = await transactionService.getUserPlannedTransaction(
+      req.user.userId,
+      plannedId,
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const updatePlannedTransactionActive = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const result = await transactionService.updatePlannedTransactionActive(
+      req.user.userId,
+      req.body.isActive,
+      req.body.plannedTransactionId,
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const updatePlannedTransaction = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json("Unauthorized");
+    }
+
+    const { plannedId } = req.query as { plannedId: string };
+    const result = await transactionService.updatePlannedTransaction(
+      req.user.userId,
+      req.body.categoryId,
+      req.body.amount,
+      req.body.description,
+      req.body.type,
+      req.body.startDate,
+      req.body.frequency,
+      plannedId,
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const deleteControl = async (
+  req: Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json("Unauthorized");
+    }
+
+    const { plannedId } = req.query as { plannedId: string };
+
+    const result = await transactionService.deletePlannedTransaction(
+      req.user.userId,
+      plannedId,
+    );
+
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
   }
 };
