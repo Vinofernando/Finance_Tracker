e5437f529f97f30c599c2bfa275818c5d803e885
Vino <vinofernando84@gmail.com>
2026-06-14 16:53:08 +0700

    add route to planned transaction service controller

--- Backend/routes/transactionRoutes.ts
+++ Backend/routes/transactionRoutes.ts
@@ -4,9 +4,15 @@ import {
   deleteTransaction,
   sumTransactions,
   getUser,
+  planningTransaction,
+  userPlanedtransaction,
+  updatePlannedTransactionActive,
+  updatePlannedTransaction,
+  deleteControl,
 } from "../controllers/transactionController.js";
 import { authenticatedToken } from "../middleware/authenticatedToken.js";
 import authorizeRole from "../utils/authorizeRole.js";
+import { predict } from "../controllers/transactionController.js";
 import express from "express";
 
 const router = express.Router();
@@ -16,5 +22,18 @@ router.post("/add-transaction", authenticatedToken, newTransaction);
 router.delete("/delete-transaction/:id", authenticatedToken, deleteTransaction);
 router.get("/summary", authenticatedToken, sumTransactions);
 router.get("/users", authenticatedToken, authorizeRole("admin"), getUser);
-
+router.post("/predict", authenticatedToken, predict);
+router.post("/planned-transaction", authenticatedToken, planningTransaction);
+router.get("/planned-transaction", authenticatedToken, userPlanedtransaction);
+router.put(
+  "/update-planned-transaction-active",
+  authenticatedToken,
+  updatePlannedTransactionActive,
+);
+router.put(
+  "/update-planned-transaction",
+  authenticatedToken,
+  updatePlannedTransaction,
+);
+router.delete("/delete-planned-transaction", authenticatedToken, deleteControl);
 export default router;
